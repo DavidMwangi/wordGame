8d1b08ba3829220f9536d3a2c9da71c9587a6d19
David Mwangi <David Mwangi>
2017-02-25 13:01:02 +0300

    Added calculateHandlen function

--- wordGame.py
+++ wordGame.py
@@ -200,10 +200,18 @@ def calculateHandlen(hand):
     """ 
     Returns the length (number of letters) in the current hand.
     
-    hand: dictionary (string-> int)
+    hand: dictionary (string int)
     returns: integer
     """
     # TO DO... <-- Remove this comment when you code this function
+    
+    total = 0
+    
+    for key in hand:
+    
+        total += hand[key]
+        
+    return total
 
 
 
